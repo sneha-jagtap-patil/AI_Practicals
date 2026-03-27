6f1655f8ab1499b35bfce57b21673b5f46d39258
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-27 08:59:41 +0530

    Added R language files

---
+++ R_Language/MyFirstCode.R
@@ -0,0 +1,38 @@
+#Topic 1: Variable Writing Techniques / Methods
+
+age <- 20
+
+#variable 1
+name <-"vivek"
+
+city <-"Pune"
+
+#variable 2
+setup_PC = "GTA 6"
+
+pi = 3.14159
+
+#Variable 3
+"india" -> country
+
+
+
+#Topic 2: Strings
+company <-"SpaceX"
+role <-"Data Analyst"
+
+# Goal to Run: "i work at SpaceX as Data Analyst"
+
+paste("I work at", company,"as",role)
+
+
+#Working with Datasets
+data()
+data(iris)
+head(iris)
+tail(iris)
+summary(iris)
+View(iris)
+
+#view ----WRONG (Lowercase v)
+#View ----Correct (Uppercase V)
