709e0a6cf00dd84524a4a1f3929d80e31d30b8b9
sneha jagtap patil <sapatil2115@gmail.com>
2026-04-11 12:30:53 +0530

    updated file

--- Big_data_tech/unit5full/03_total_revenue.py
+++ Big_data_tech/unit5full/03_total_revenue.py
@@ -5,10 +5,10 @@ print("Step 3: Total Revenue")
 con = duckdb.connect("taxi.db")
 
 result = con.execute(""" 
-SELECT SUM(total) AS total_revenue
+SELECT SUM(total) AS total_revenueS
 FROM taxi
 """).fetchdf()
 
 print(result)
 
-con.close() ##total 
+con.close() 
