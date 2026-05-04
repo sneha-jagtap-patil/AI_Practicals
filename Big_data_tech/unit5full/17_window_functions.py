9910a042c8fc0b3e26cac60977470eacb42bd117
sneha jagtap patil <sapatil2115@gmail.com>
2026-05-04 13:02:45 +0530

    add files

--- Big_data_tech/unit5full/17_window_functions.py
+++ Big_data_tech/unit5full/17_window_functions.py
@@ -3,7 +3,7 @@ import duckdb
 import pandas as pd
 
 # ✅ Fix truncated output
-pd.set_option('display.max_rows', None)        # show all rows
+pd.set_option('display.max_rows', None)        
 pd.set_option('display.max_columns', None)     # show all columns
 pd.set_option('display.width', None)           # auto width
 pd.set_option('display.max_colwidth', None)    # full column content
