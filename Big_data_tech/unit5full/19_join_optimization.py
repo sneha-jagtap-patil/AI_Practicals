0bd49bbf24d656dc8de0902e763c8e12e3d82231
sneha jagtap patil <sapatil2115@gmail.com>
2026-05-04 13:06:40 +0530

    add files

--- Big_data_tech/unit5full/19_join_optimization.py
+++ Big_data_tech/unit5full/19_join_optimization.py
@@ -4,7 +4,7 @@ print("Join Optimization")
 
 con = duckdb.connect("taxi.db")
 
-#Helper Table
+##Helper Table
 con.execute("""
 CREATE OR REPLACE TABLE payment_info AS
 SELECT DISTINCT payment AS type            
